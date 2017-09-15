6ed68f3eb342289673a557d93a7bcdd2491642c6
Liam <33014212@student.murdoch.edu.au>
2017-09-16 00:10:52 +0800

    Added Doxygen Comments to Wrold.h

--- assimp_test/World.h
+++ assimp_test/World.h
@@ -6,37 +6,184 @@
 #include "Camera.h"
 #include "Singleton.h"
 
+
+//---------------------------------------------------------------------------------
+
+/**
+* @class World
+* @brief  Manages the running of the game world
+*
+* World is responsible for the running of the game world, this consists of
+* Initialising the world, Running the world, Updating the wolrd. It also
+* is curently responsible for the location of objects within the scene
+*
+* @author Liam Goerke
+* @version 01
+* @date 10/09/2017
+*
+* @author ChengYu Yang
+* @version 02
+* @date 15/09/2017 
+*
+*
+*
+*/
+
 class World{
 public:
+	/**
+	* @brief  Constructor
+	*
+	* Initialises game_status singleton and sets game status to GAME_PLAYING
+	*/
 	World(){
 		game_status = Singleton<GameStatus>::Instance();
 		*game_status = GAME_PLAYING;
 		camera = Singleton<Camera>::Instance();
 	};
+	/**
+	* @brief  Destructor
+	*/
 	~World();
+	/**
+	* @brief  Copy constructor
+	*/
 	World(const World & cpy){};
 
+	/**
+	* @brief  Initializes the game world
+	* This function loads initial models, sets bounding boxes and sets the players initial values
+	* @return void
+	*/
 	void InitializeGame();
+
+	/**
+	* @brief  Runs the simulation world
+	* Calls the graphics handler to create a game windwo
+	* @param api - a string to determine which graphics to use (currently OpenGL)
+	* @return void
+	*/
 	void RunGame(const char* api);
+
+	/**
+	* @brief  Updates the world 
+	* Checks the curent game status and reacts accordingly
+	* Makes calls to render models, update player values and checks for collisisons
+	* @return void
+	*/
 	void UpdateGame();
+
+	/**
+	* @brief  Willl be responsible for destruction of game
+	* 
+	* @return void
+	*/
 	void GameDestruction(){};
 	
-	void InitialBench1();
-	void InitialBench2();
-	void InitialChair1();
-	void InitialChair2();
-	void InitialTable1();
+	/**
+	* @brief  Loads the scene
+	* Loads the scene model
+	* 
+	* @return void
+	*/
 	void InitialScene();
-	void Initial2DTexture();
 
-	void DrawBench1();
-	void DrawBench2();
-	void DrawChair1();
-	void DrawChair2();
-	void DrawTable1();
+	/**
+	* @brief  Loads model into world
+	*
+	* @return void
+	*/
+	void InitialBench1();
+
+	/**
+	* @brief  Loads model into world
+	*
+	* @return void
+	*/
+	void InitialBench2();
+
+	/**
+	* @brief  Loads model into world
+	*
+	* @return void
+	*/
+	void InitialChair1();
+
+	/**
+	* @brief  Loads model into world
+	*
+	* @return void
+	*/
+	void InitialChair2();
+
+	/**
+	* @brief  Loads model into world
+	*
+	* @return void
+	*/
+	void InitialTable1();
+	
+	/**
+	* @brief  Loads 2D exit texture on screen
+	*
+	* @return void
+	*/
+	void Initial2DTexture();
+	
+	/**
+	* @brief  Loads model into world
+	*
+	* @return void
+	*/
 	void DrawScene();
 
+	/**
+	* @brief  Draws Scene
+	*
+	* @return void
+	*/
+	void DrawBench1();
+
+	/**
+	* @brief  Loads model into world
+	*
+	* @return void
+	*/
+	void DrawBench2();
+
+	/**
+	* @brief  Loads model into world
+	*
+	* @return void
+	*/
+	void DrawChair1();
+
+	/**
+	* @brief  Loads model into world
+	*
+	* @return void
+	*/
+	void DrawChair2();
+
+	/**
+	* @brief  Loads model into world
+	*
+	* @return void
+	*/
+	void DrawTable1();
+
+	/**
+	* @brief  Sets the scenes bounding walls
+	*
+	* @return void
+	*/
 	void SetBoundingWall();
+
+	/**
+	* @brief  Checks for collissions between player and AABB's
+	*
+	* @return void
+	*/
 	void CheckBoundingBox();
 
 private:
