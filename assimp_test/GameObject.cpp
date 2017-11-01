55497e92e4947c941d7c6ed7a5a99bc64e408f05
32581649 <skywingx123@gmail.com>
2017-11-01 13:47:36 +0800

    try to commit with new physics
    try if not working convert back

--- assimp_test/GameObject.cpp
+++ assimp_test/GameObject.cpp
@@ -1,29 +1,37 @@
 #include "GameObject.h"
 
-//AABB GameObject::boundingBox;
+#include "World.h"
 
 GameObject::GameObject()
 { 
-	bulletBox = new btCollisionObject();
+	bulletBox = new OObtCollisionObject();
+	isCollided = false;
+	btDelay = 0.0;
 	Pos.x = 0; Pos.y = 0; Pos.z = 0;
+	posBeforeCollide[0] = Pos;
+	posBeforeCollide[1] = Pos;
 	Sca.x = 1; Sca.y = 1; Sca.z = 1;
 	Rot.x = 0; Rot.y = 1.0; Rot.z = 0; Rot.w = 0;
 	Vel.x = 0; Vel.y = 0; Vel.z = 0;
-	AngV.x = 0; AngV.y = 0; AngV.z = 0;
-	mass = 10;
+	AngVaxis.x = 0; AngVaxis.y = 0; AngVaxis.z = 0;
+	AngV = 0.0f;
+	Acceleration = .0f;
+	linearLoss = 0.000; angLoss = 0.05;
+	linearLimit = 1.f; AngLimit = 1.f;
+	mass = 0.0f;
 	COM = Pos;
-	sceneBody = false;
 }
 
 void GameObject::Load(Graphics *graphics_handler,string fname)
 {
 	pathName = fname;
 	graphics_handler->LoadModel(fname);
-	
 }
 
 void GameObject::Render(Graphics *graphics_handler)
 {
+	World* world_instance = Singleton<World>::Instance();
+	this->UpdateObject(world_instance->GetDeltaTime());
 	graphics_handler->RenderModel(pathName,Pos, Sca, Rot);
 }
 
@@ -49,9 +57,9 @@ void GameObject::Rotate(float x, float y, float z,float degree)
 	Rot.y = y;
 	Rot.z = z;
 	Rot.w = degree;
+	
 	if (bulletBox){
-		btQuaternion quat;
-		quat.setRotation(btVector3(x, y, z), glm::radians(degree));
+		btQuaternion quat(btVector3(x, y, z), btRadians(degree));
 		bulletBox->getWorldTransform().setRotation(quat);
 	}
 }
@@ -82,7 +90,7 @@ void GameObject::SetBoundingBox(float x, float y, float z)
 	BoxSize.z = z;
 }
 
-btCollisionObject* GameObject::SetBulletBoundingBox(float size_x, float size_y, float size_z){
+OObtCollisionObject* GameObject::SetBulletBoundingBox(float size_x, float size_y, float size_z){
 
 
 	if (!bulletBox) {
@@ -114,18 +122,52 @@ bool GameObject::processCollision(GameObject &obj, glm::vec3 *collisionPoint)
 
 void GameObject::UpdateObject(double deltaTime)
 {
+	if (isCollided){
+		btDelay += deltaTime;
+		if (btDelay > 1.0){
+			btDelay = 0;
+			isCollided = false;
+		}
+	}
+	//Vel += Acceleration*deltaTime;
+	// limit vel
+	/*if (Vel.x > 1.0f) Vel.x = 1.0;
+	else if (Vel.x < -1.0f) Vel.x = -1.0;
+	if (Vel.y > 1.0f) Vel.y = 1.0;
+	else if (Vel.y < -1.0f) Vel.y = -1.0;
+	if (Vel.z > 1.0f) Vel.z = 1.0;
+	else if (Vel.z < -1.0f) Vel.z = -1.0;*/
+
 	Postition(Pos.x + Vel.x * deltaTime, 
 		Pos.y + Vel.y * deltaTime, 
 		Pos.z + Vel.z * deltaTime);
+	// loss
+	if (Vel.x > .0f) Vel.x -= linearLoss;
+	else if (Vel.x < .0f) Vel.x += linearLoss;
+	if (Vel.y > .0f) Vel.y -= linearLoss;
+	else if (Vel.y < .0f) Vel.y += linearLoss;
+	if (Vel.z > .0f) Vel.z -= linearLoss;
+	else if (Vel.z < .0f) Vel.z += linearLoss;
+
 
 	//cout << "position y:" << Pos.y << endl;
+	glm::vec3 AngVaxisDt;
+	AngVaxisDt.x = AngVaxis.x * deltaTime;
+	AngVaxisDt.y = AngVaxis.y * deltaTime;
+	AngVaxisDt.z = AngVaxis.z * deltaTime;
+	glm::vec4 newRot = Physics::AngularVelToRotQuat(AngVaxis);
+	
+	
+	//AngV += Acceleration*deltaTime;
+	////Rotate(AngVaxis.x, AngVaxis.y, AngVaxis.z, Rot.w + AngV * deltaTime);
+	Rotate(newRot.x, newRot.y, newRot.z, Rot.w + newRot.w * deltaTime*30);
+	// loss
+	/*if (AngVaxis.x > .0f) AngVaxis.x -= angLoss;
+	else if (AngVaxis.x < .0f) AngVaxis.x += angLoss;
+	if (AngVaxis.y > .0f) AngVaxis.y -= angLoss;
+	else if (AngVaxis.y < .0f) AngVaxis.y += angLoss;
+	if (AngVaxis.z > .0f) AngVaxis.z -= angLoss;
+	else if (AngVaxis.z < .0f) AngVaxis.z += angLoss;*/
 
-	glm::vec3 angVdt; 
-	glm::vec4 newRot;
-	angVdt.x = AngV.x * deltaTime;
-	angVdt.y = AngV.y * deltaTime;
-	angVdt.z = AngV.z * deltaTime;
-	newRot = Physics::AngularVelToRotQuat(angVdt);
-
-	Rotate(newRot.x, newRot.y, newRot.z, Rot.w + newRot.w);
+	SetPosBeforeCollide(Pos);
 }
