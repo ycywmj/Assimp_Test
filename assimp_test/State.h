fff5dc7e552f7f99576e8e172bb7ecc51cd9062c
Kin Mak <32581649@student.murdoch.edu.au>
2017-11-18 15:42:09 +0800

    Doxygen comment progress 80% Leon next commit will finish 100%

--- assimp_test/State.h
+++ assimp_test/State.h
@@ -1,16 +1,38 @@
 #ifndef __STATE_H
 #define __STATE_H
 
+/**
+* @class State
+* @brief State class for state machine
+*
+* @author ChengYu Yang
+* @version 01
+* @date 25/10/2017
+*
+*
+*/
 template <class entity_type>
 class State{
 public:
+	/**
+	* @brief  destructor
+	*/
 	virtual ~State(){}
 
-	//this will execute when the state is entered
+	/**
+	* @brief  this will execute when the state is entered
+	* @param  state variable
+	*/
 	virtual void Enter(entity_type*) = 0;
-	//this is the state's normal update function
+	/**
+	* @brief  this is the state's normal update function
+	* @param  state variable
+	*/
 	virtual void Execute(entity_type*) = 0;
-	//this will execute when the state is exited
+	/**
+	* @brief  this will execute when the state is exited
+	* @param  state variable
+	*/
 	virtual void Exit(entity_type*) = 0;
 };
 
